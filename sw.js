9ba3cfc38dea731b4b139acc7195623be9117d3e
HenriqueCallazans <luizhcsocial@gmail.com>
2026-03-24 12:19:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
 const CACHE = 'geladeira-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
@@ -12,6 +20,20 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(cached => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(e.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE).then(cache => {
+          cache.put(e.request, responseToCache);
+        });
+        return response;
+      });
+    })
   );
 });
